c6d8884e4c9dabd0b499f6cd50427e977265450b
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-15 15:58:45 +0530

    greatest of three

---
+++ max_num.js
@@ -0,0 +1,17 @@
+//input 3 numbers
+let a = 10;
+let b = 20;
+let c = 5;
+
+//greatest of 3 numbers
+if(a > b && a > c){
+    console.log('a is greater');
+}
+
+else if(a<b && b>c){
+    console.log('b is greater');
+}
+
+else{
+    console.log('c is greater');
+}
