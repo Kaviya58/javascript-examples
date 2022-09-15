584b241db5c9552ab49de30e3139db92e83d433f
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-15 14:53:06 +0530

    swap two variables

---
+++ swap.js
@@ -0,0 +1,8 @@
+let var1 = 5;
+let var2 = 10;
+console.log(`Before swapping : var1 = ${var1} , var2 = ${var2}`);
+
+let temp = var1;
+var1 = var2;
+var2 = temp;
+console.log(`After swapping : var1 = ${var1} , var2 = ${var2}`);
