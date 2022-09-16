b55678dbac9c9132931cc79fb8967ec6ab24d05a
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-16 11:09:16 +0530

    Checking last digits

---
+++ check_last_digit.js
@@ -0,0 +1,11 @@
+let num1 = 4;
+let num2 = 524;
+
+rem1 = num1 % 10;
+rem2 = num2 % 10;
+if(rem1 === rem2)
+    console.log(`${num1} and ${num2} have the same last digit.`);
+
+else
+    console.log(`${num1} and ${num2} have different last digit.`)
+
