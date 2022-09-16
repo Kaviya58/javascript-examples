99a0e9a6a31353520c0bbc895d92a675f959e9b4
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-16 18:15:27 +0530

    start and end character

---
+++ start_end.js
@@ -0,0 +1,9 @@
+let str = 'Javascript';
+let newStr = str.toLowerCase();
+
+if(newStr.startsWith('j') && newStr.endsWith('t')){
+    console.log(`The string starts with 'J' and ends with 'T'.`);
+}
+else{
+    console.log(`The string does not start with 'J' and ends with 'T'.`);
+}
