ca3b53c9b319dfb913ea88d6a4de1566306bc962
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-17 10:44:54 +0530

    compare two strings

---
+++ compare_string.js
@@ -0,0 +1,10 @@
+const str1 = 'java script';
+const str2 = 'Java Script';
+
+//compare two strings
+if(str1.toLowerCase() === str2.toLowerCase()){
+    console.log('Two strings are same.');
+}
+else{
+    console.log('Two strings are not same.');
+}
