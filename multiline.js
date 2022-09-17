ac0c8154ccad6e50e1473330d68e26d49d3bf51c
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-17 09:18:36 +0530

    multiline string

---
+++ multiline.js
@@ -0,0 +1,6 @@
+//create multiline string
+const str = `JavaScript is a programming language.
+It was developed by Brendan Eich of Netscape in 1995.
+Node.js is a open source server environment to run JS on server.`
+
+console.log(str);
