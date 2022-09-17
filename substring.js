3a21e96d16805570d85769de1a611de3add9f805
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-17 10:39:24 +0530

    check string has substring

---
+++ substring.js
@@ -0,0 +1,9 @@
+const str = 'Have a nice day';
+const checkStr = 'nice';
+
+if(str.includes(checkStr)){
+    console.log(`The string has '${checkStr}' in it.`);
+}
+else{
+    console.log(`The string does not contain '${checkStr}' in it.`);
+}
