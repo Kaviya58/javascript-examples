7b6859a438caf8b9d365454d926183caa36ccc2b
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-17 09:55:50 +0530

    string starts with another string

---
+++ starts_with.js
@@ -0,0 +1,10 @@
+const str1 = 'Javascript is a programming language';
+const str2 = 'java';
+
+//check string starts with another string
+if(str1.toLowerCase().startsWith(str2)){
+    console.log(`The string starts with ${str2}`);
+}
+else{
+    console.log(`The string does not start with ${str2}`);
+}
