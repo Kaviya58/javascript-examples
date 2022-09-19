42aed5ef28557db3943d19df2461f1858b925c12
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-19 10:44:07 +0530

    check object ia an array

---
+++ check_array.js
@@ -0,0 +1,12 @@
+//check if an object is array or not
+
+const arr = [1,2,3,4,5];
+
+let result = Array.isArray(arr);
+
+if(result){
+    console.log(`${arr} is an array.`);
+}
+else{
+    console.log(`${arr} is an array.`);
+}
