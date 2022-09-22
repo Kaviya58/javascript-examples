bb946f5b603049d819503fed53e2ea9c4c42c56e
Kaviya58 <kaviyakarthick2000@gmail.com>
2022-09-22 12:37:41 +0530

    write to console

---
+++ console.js
@@ -0,0 +1,24 @@
+//code to write to console
+
+//passing number
+console.log(5);
+
+//passing string
+console.log('javascript');
+
+//passing variable
+let num = 10;
+console.log(num);
+
+//passing function
+function sum(a ,b){
+    return a+b;
+}
+console.log(sum(1 , 2));
+
+//passing object
+const person = {
+    name : 'Anu',
+    age : 21
+};
+console.log(person);
